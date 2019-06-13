44cf1390c9fd8b05725f7b7db166938664ef805e
BURIQ2019 <50648774+BURIQ2019@users.noreply.github.com>
2019-06-13 11:56:58 +0700

    TOOLS-COMPLETE.sh

---
+++ TOOLS-COMPLETE.sh
@@ -0,0 +1,121 @@
+#MAU NGAPAIN ZEYENG
+#MAU UBAH MAU GUE CIPOK LU KALO UBAH
+#SALAM MR.T
+
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+echo "##################################"|lolcat
+echo "/ TOOLS : KUMPULAN TOOLS            /"|lolcat
+echo "/ CODED : MR.T               /"|lolcat
+echo "/ TEAM  : GHOSTARMYCYBER  /"|lolcat
+echo "##################################"|lolcat
+
+echo "INSTALL NOMOR 1 DULU IYA GANS/CANS"
+echo "[1] BAHAN TOOLS"
+echo "[2] TOOLS HACK FB"
+echo "[3] SPAM CALL"
+echo "[4] SPAM GRAB"
+echo "[5] PERKIRAN CUACA"
+echo "[6] DEFACE"
+echo "[7] TOOLS GEMPA"
+echo "[8] SPAM TRI"
+echo "[9] Keluar"
+read -p "PILIH SALAH SATU : " bro
+
+if [ $bro = 1 ] 
+then
+pkg install git
+pkg install python
+pkg install python2
+pip2 install requests mechanize
+pip install requests
+apt  install curl
+apt install libcurl
+pkg install php
+apt install bash 
+apt install lolcat
+apt install toilet 
+echo "\033[31;1mSUDAH SIAP INSTALL TOOLSNYA"
+sleep 1
+sh TOOLS-COMPLETE.sh
+fi
+
+if [ $bro = 2 ] 
+then
+git clone https://github.com/BURIQ2019/Tools-Fb
+echo "\033[31;1mCARA PAKAI TOOLS INI BUKA TAB BARU TERUS cd Tools-Fb TERUS python2 Tools-FB.py"
+sleep 1
+sh TOOLS-COMPLETE.sh
+fi
+
+if [ $bro = 3 ] 
+then
+git clone https://github.com/BURIQ2019/SpamCall2019
+echo "\033[31;1mCARA PAKAI TOOLS INI BUKA TAB BARU TERUS cd SpamCall2019 TERUS php call.php"
+sleep 1
+sh TOOLS-COMPLETE.sh
+fi
+
+if [ $bro = 4 ] 
+then
+git clone https://github.com/BURIQ2019/SpamGrab2019
+echo "\033[31;1mCARA PAKAI TOOLS INI BUKA TAB BARU TERUS cd SpamGrab2019 TERUS python grab.py"
+sleep 1
+sh TOOLS-COMPLETE.sh
+fi
+
+if [ $bro = 5 ] 
+then
+git clonehttps://github.com/BURIQ2019/Perkirain-Cuaca
+echo "\033[31;1mCARA PAKAI TOOLS INI BUKA TAB BARU TERUS cd Perkirain-Cuaca TERUS sh CUACA.sh"
+sleep 1
+sh TOOLS-COMPLETE.sh
+fi
+
+if [ $bro = 6 ] 
+then
+git clone https://github.com/BURIQ2019/Tools-Deface2019
+echo "\033[31;1mCARA PAKAI TOOLS INI BUKA TAB BARU TERUS cd Tools-Deface2019TERUS sh Tools-Deface.sh"
+sleep 1
+sh TOOLS-COMPLETE.sh
+fi
+
+if [ $bro = 7 ] 
+then
+git clonehttps://github.com/BURIQ2019/GEMPA-TERKINI
+echo "\033[31;1mCARA PAKAI TOOLS INI BUKA TAB BARU TERUS cd GEMPA-TERKINI TERUS php Gempa-Terkini.php"
+sleep 1
+sh TOOLS-COMPLETE.sh
+fi
+
+if [ $bro = 8 ] 
+then
+git clone https://github.com/BURIQ2019/SpamSms
+echo "\033[31;1mCARA PAKAI TOOLS INI BUKA TAB BARU TERUS cd SpamSms TERUS python SpamSms.py"
+sleep 1
+sh TOOLS-COMPLETE.sh
+fi
+
+if [ $bro = 9 ] 
+then
+echo "\033[31;1mGHOST ARMY CYBER"
+sleep 1
+echo "\033[32;1mTerimakasih Sudah Menggunakan Tools ini"
+sleep 1
+echo "\033[35;1mSelamat Beraktivitas"
+sleep 1
+exit
+fi
